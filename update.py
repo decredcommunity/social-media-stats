ac2a8bf021bbfc3fa6c13525cca40aa883d22f2a
xaur <24484727+xaur@users.noreply.github.com>
2020-10-02 23:27:31 +0000

    update.py: add script to update subrepos

---
+++ update.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+from os.path import isdir, join
+from subprocess import call
+
+def update_subrepo(url, branch, path):
+    if isdir(path):
+        gitpath = join(path, ".git")
+        if isdir(gitpath):
+            print("Updating subrepository:", path)
+            call(["git", "--git-dir", gitpath, "--work-tree", path,
+                  "pull", "--ff-only"])
+        else:
+            print("Unexpected state: dir '{}' exists but does not contain"
+                  " .git dir".format(path))
+    else:
+        print("Initializing subrepository:", path)
+        call(["git", "clone", "--single-branch", "--branch", branch, url,
+              path])
+
+REPO = "https://github.com/decredcommunity/social-media-stats.git"
+
+update_subrepo(REPO, "data", "data")
+update_subrepo(REPO, "graphs", "graphs")
